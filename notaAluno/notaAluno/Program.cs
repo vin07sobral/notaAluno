5e25bd8756644d53fd17cfae97e34b07f105b6dc
Marcus Vinicius Sobral <mvinicius.sobral@gmail.com>
2018-09-26 16:15:41 -0300

    Add files via upload

---
+++ notaAluno/notaAluno/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+namespace nota_aluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double N1, N2, N3, soma;
+            string[] vet = Console.ReadLine().Split(' ');
+            N1 = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            N2 = double.Parse(vet[1], CultureInfo.InvariantCulture);
+            N3 = double.Parse(vet[2], CultureInfo.InvariantCulture);
+            soma = N1 + N2 + N3;
+            Console.WriteLine(soma.ToString("F2", CultureInfo.InvariantCulture));
+            if (soma < 60.0)
+            {
+                Console.WriteLine("DEPENDENCIA");
+            }
+            Console.ReadLine();
+        }
+    }
+}
